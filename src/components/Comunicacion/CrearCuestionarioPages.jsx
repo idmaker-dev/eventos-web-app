a2ca6b61961b72929204e869f8c256b771e9c71b
UIFROILAN\froil <froilan.surozz.6a97@gmail.com>
2025-09-23 16:08:49 -0600

    Boton para mostrar o ocultar formulario

--- src/components/Comunicacion/CrearCuestionarioPages.jsx
+++ src/components/Comunicacion/CrearCuestionarioPages.jsx
@@ -2,7 +2,7 @@ import { Button, Field, Input, Label, Textarea } from "@headlessui/react";
 import clsx from "clsx";
 import React, { useState } from "react";
 import FormularioCuestionario from "./FormularioCuestionario";
-import { LaptopIcon, Smartphone, Plus, Minus } from "lucide-react";
+import { LaptopIcon, Smartphone, Plus, Minus, ChevronRight, ChevronLeft } from "lucide-react";
 import ModalConfimacion from "./ModalConfimacion";
 import { DeviceFrameset } from "react-device-frameset";
 import "react-device-frameset/styles/marvel-devices.min.css";
@@ -17,6 +17,7 @@ export default function CrearCuestionarioPages() {
   const [otra, setOtra] = useState("");
   const [modoVista, setModoVista] = useState("laptop"); // "laptop" o "telefono"
   const [modalOpen, setModalOpen] = useState(false);
+  const [Ocultar, setOcultar] = useState(false);
 
   const handleChange = (key, delta) => {
     setRestricciones((prev) => ({
@@ -27,7 +28,8 @@ export default function CrearCuestionarioPages() {
 
   return (
     <div className="grid grid-cols-1  lg:grid-cols-3 md:gap-0 ">
-      <div className="md:col-span-1 relative  ">
+      <div className={clsx("md:col-span-1 relative", Ocultar && "hidden")}>
+        
         <div className="h-full  lg:h-[72vh] mb-14 md:mb-14 lg:mb-0 overflow-y-auto pr-2">
           <div className="p-4 border-b-2">
             <h2 className="text-lg font-semibold  text-casal dark:text-Acapulco mb-0">
@@ -272,46 +274,58 @@ export default function CrearCuestionarioPages() {
         </div>
       </div>
       {/* <   Presentaciones del formulario */}
-      <div className="md:col-span-2  h-[85vh] overflow-y-auto  lg:border-l-2 bg-[#5c8287] dark:bg-[#2a9d8f]/5 rounded-r-xl p-4">
+      <div className={clsx("md:col-span-2  h-[85vh] overflow-y-auto  lg:border-l-2 bg-[#5c8287] dark:bg-[#2a9d8f]/5 rounded-r-xl p-4 relative", Ocultar ? "md:col-span-3 rounded-l-xl " : "md:col-span-2",)}>
+        <div className="absolute top-0 left-0 flex space-x-2 z-10">
+          <Button
+            onClick={() => setOcultar(!Ocultar)}
+            className={clsx("p-2 bg-casal text-white hover:bg-casal/80 transition shadow-sm relative group", Ocultar ? "rounded-br-lg rounded-tl-xl " : "rounded-br-lg")}
+          >
+             {!Ocultar ? <ChevronLeft className="h-5 w-5" /> : <ChevronRight className="h-5 w-5" />}
+             {Ocultar ? 
+              <span className="absolute left-16 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">Mostrar formulario</span> : 
+              <span className="absolute left-16 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">Ocultar formulario</span>
+             }
+          </Button>
+        </div>
         <div className="flex-col lg:flex-row lg:space-x-4 lg:space-y-0 space-y-4 flex">
           {/* estos son los detalles del cuestionario pero con los botones se puede hacer para modal celular y laptop */}
           <div className="flex justify-center gap-3 mb-4 lg:hidden">
-        <div className="relative group">
-          <Button
-            onClick={() => setModoVista("laptop")}
-            className={clsx(
-              "text-white px-3 py-1 rounded",
-              "focus:outline-none transition",
-              modoVista === "laptop" ? "bg-Acapulco" : "bg-casal"
-            )}
-          >
-            <LaptopIcon className="w-6 h-6" />
-          </Button>
-          <span className="absolute left-1/2 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">
-            Vista laptop
-          </span>
-        </div>
-        <div className="relative group">
-          <Button
-            onClick={() => setModoVista("telefono")}
-            className={clsx(
-              "text-white px-3 py-1 rounded",
-              "focus:outline-none transition",
-              modoVista === "telefono" ? "bg-Acapulco" : "bg-casal"
-            )}
-          >
-            <Smartphone className="w-6 h-6" />
-          </Button>
-          <span className="absolute left-1/2 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">
-            Vista teléfono
-          </span>
-        </div>
-         <div className="">
+            <div className="relative group">
+              <Button
+                onClick={() => setModoVista("laptop")}
+                className={clsx(
+                  "text-white px-3 py-1 rounded",
+                  "focus:outline-none transition",
+                  modoVista === "laptop" ? "bg-Acapulco" : "bg-casal"
+                )}
+              >
+                <LaptopIcon className="w-6 h-6" />
+              </Button>
+              <span className="absolute left-1/2 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">
+                Vista laptop
+              </span>
+            </div>
+            <div className="relative group">
+              <Button
+                onClick={() => setModoVista("telefono")}
+                className={clsx(
+                  "text-white px-3 py-1 rounded",
+                  "focus:outline-none transition",
+                  modoVista === "telefono" ? "bg-Acapulco" : "bg-casal"
+                )}
+              >
+                <Smartphone className="w-6 h-6" />
+              </Button>
+              <span className="absolute left-1/2 -translate-x-1/2 -bottom-8 z-10 whitespace-nowrap rounded bg-gray-800 px-2 py-1 text-xs text-white opacity-0 group-hover:opacity-100 transition">
+                Vista teléfono
+              </span>
+            </div>
+            <div className="">
               <Button className="bg-casal  w-full text-white px-4 py-1 rounded hover:bg-Acapulco transition">
                 Enlace de cuestionario
               </Button>
             </div>
-      </div>
+          </div>
           <div className="w-full ">
             <div
               className={clsx(
@@ -321,11 +335,16 @@ export default function CrearCuestionarioPages() {
               <DeviceFrameset
                 device={modoVista === "laptop" ? "MacBook Pro" : "iPhone X"}
                 color={modoVista === "laptop" ? "silver" : "black"}
-                height={modoVista === "laptop" ? 600 : 700}
+                height={modoVista === "laptop" ? 600 : 750}
                 width={modoVista === "telefono" ? 370 : undefined}
                 landscape={modoVista === "laptop"}
               >
-                <div className={clsx("h-full overflow-y-auto ", modoVista === "telefono" ? "max-h-[800px]" : "max-h-[600px]")}>
+                <div
+                  className={clsx(
+                    "h-full overflow-y-auto ",
+                    modoVista === "telefono" ? "max-h-[800px]" : "max-h-[600px]"
+                  )}
+                >
                   <FormularioCuestionario />
                 </div>
               </DeviceFrameset>
@@ -333,7 +352,6 @@ export default function CrearCuestionarioPages() {
           </div>
           {/* botones para cambiar vista Modo*/}
           <div className="w-full lg:w-40 lg:space-y-3 flex lg:flex-col gap-3 lg:block hidden">
-            
             <div className="flex justify-center gap-3">
               <div className="relative group">
                 <Button
